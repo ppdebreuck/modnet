ac9c890b48c18f97eb00f4ee9c6063b0777c012a
Matthew Evans <me388@cam.ac.uk>
2020-10-22 16:42:25 +0100

    Added simple modnet model tests

---
+++ modnet/tests/test_model.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+import pytest
+
+def test_train_small_model_single_target(subset_moddata):
+    """Tests the single target training."""
+    from modnet.models import MODNetModel
+
+    data = subset_moddata
+    # set 'optimal' features manually
+    data.optimal_features = [
+        col for col in data.df_featurized.columns if col.startswith("ElementProperty")
+    ]
+
+    model = MODNetModel(
+        [[["eform"]]],
+        weights={"eform": 1},
+        num_neurons=([16], [8], [8], [4]),
+        n_feat=10,
+    )
+
+    model.fit(data, epochs=5)
+    model.predict(data)
+
+
+def test_train_small_model_multi_target(subset_moddata):
+    """Tests the multi-target training."""
+    from modnet.models import MODNetModel
+
+    data = subset_moddata
+    # set 'optimal' features manually
+    data.optimal_features = [
+        col for col in data.df_featurized.columns if col.startswith("ElementProperty")
+    ]
+
+    model = MODNetModel(
+        [[["eform", "egap"]]],
+        weights={"eform": 1, "egap": 1},
+        num_neurons=([16], [8], [8], [4]),
+        n_feat=10,
+    )
+
+    model.fit(data, epochs=5)
+    model.predict(data)
+
+
+def test_train_small_model_presets(subset_moddata):
+    """Tests the `fit_preset()` method."""
+    from modnet.model_presets import MODNET_PRESETS
+    from modnet.models import MODNetModel
+
+    for ind, preset in enumerate(MODNET_PRESETS):
+        MODNET_PRESETS[ind]["epochs"] = 5
+
+    data = subset_moddata
+    # set 'optimal' features manually
+    data.optimal_features = [
+        col for col in data.df_featurized.columns if col.startswith("ElementProperty")
+    ]
+
+    model = MODNetModel(
+        [[["eform", "egap"]]],
+        weights={"eform": 1, "egap": 1},
+        num_neurons=([16], [8], [8], [4]),
+        n_feat=10,
+    )
+
+    model.fit_preset(data)
+
+@pytest.mark.skip(msg="Until pickle bug is fixed")
+def test_model_integration(subset_moddata):
+    """Tests training, saving, loading and predictions."""
+    from modnet.models import MODNetModel
+
+    data = subset_moddata
+    # set 'optimal' features manually
+    data.optimal_features = [
+        col for col in data.df_featurized.columns if col.startswith("ElementProperty")
+    ][:10]
+
+    model = MODNetModel(
+        [[["eform", "egap"]]],
+        weights={"eform": 1, "egap": 1},
+        num_neurons=([16], [8], [8], [4]),
+        n_feat=10,
+    )
+
+    model.fit(data, epochs=5)
+
+    model.save("test")
+    loaded_model = MODNetModel.load("test")
+
+    assert model.predict(data) == loaded_model.predict(data)
