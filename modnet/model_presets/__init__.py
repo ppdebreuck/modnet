79410bb9fb8d686e80814dad77843a5c508b83ef
Pierre-Paul De Breuck <pierre-paul.debreuck@uclouvain.be>
2020-11-05 12:25:04 +0100

    Automated validation procedure for hyper parameters (#18)

---
+++ modnet/model_presets/__init__.py
@@ -0,0 +1,7 @@
+""" This submodule defines some MODNet model presets which are used
+    in the fit_preset function.
+"""
+
+from .presets import hyperparam_presets
+
+MODNET_PRESETS = hyperparam_presets
---
+++ modnet/model_presets/presets.py
@@ -0,0 +1,21 @@
+batch_sizes = [64]
+
+learning_rates = [0.02, 0.005]
+
+archs = [(350,[[64],[8],[8],[]]),(350,[[256],[128],[8],[8]]),(1000,[[256],[128],[8],[]])]
+
+epochs = [1000]
+
+losses = ['mse']
+
+activations = ['elu']
+
+hyperparam_presets = []
+for bs in batch_sizes:
+    for lr in learning_rates:
+        for a in archs:
+            for e in epochs:
+                for l in losses:
+                    for act in  activations:
+                        preset = {'batch_size':bs, 'lr':lr, 'n_feat':a[0], 'num_neurons':a[1], 'epochs':e, 'loss':l, 'act':act}
+                        hyperparam_presets.append(preset)
