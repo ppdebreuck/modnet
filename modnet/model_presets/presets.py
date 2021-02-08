ce58bd00c891cae8fd019c54e28feb956fc8ef4e
Greg <gregoireheymans@gmail.com>
2021-02-08 17:55:07 +0100

    Getting back to the initial structure of the presets.py file.

--- modnet/model_presets/presets.py
+++ modnet/model_presets/presets.py
@@ -1,79 +1,34 @@
-"""This submodule defines some heuristic presets to be used as the first step
-of hyperparameter optimisation.
+batch_sizes = [64]
 
-"""
+learning_rates = [0.01, 0.001]
 
-from typing import List, Dict, Any
-import itertools
+archs = [
+    (350, [[64], [8], [8], []]),
+    (350, [[256], [128], [8], [8]]),
+    (1000, [[256], [128], [8], []]),
+    (350, [[128], [64], [32], [32]]),
+]
 
+epochs = [1000]
 
-def gen_presets(n_feat: int, n_samples: int) -> List[Dict[str, Any]]:
-    """ "Generates sensible preset architectures and learning parameters
-    based on number of samples and features:
+losses = ["mse"]
 
-      * Small limit, e.g. 100 features and 1000 samples yields architectures
-        - [200, 100, 25, 25]
-        - [50, 50, 25, 25].
+activations = ["elu"]
 
-      * Crossover, e.g. 100 features, ~2000 samples yields:
-
-        If just above 2000 samples:
-
-        - [200, [100, 100], 25, 25]
-        - [50, [50, 50], 25, 25]
-
-        or if just below 2000 samples:
-
-        - [200, 100, 25, 25]
-        - [50, 50, 25, 25].
-
-      * Large limit, e.g. 500 features, 50000 samples yields architectures
-        - [1000, [500, 500], 125, 125]
-        - [250, [250, 250], 125, 125]
-
-    Arguments:
-        n_feat: The number of training features available to the model.
-        n_samples: The number of training samples available to the model.
-
-    Returns:
-        List of dictionaries to pass as keywords to `model.fit(...)`.
-
-    """
-
-    batch_sizes = [32, 64]
-    learning_rates = [0.01, 0.005]
-    epochs = [500]
-    losses = ["mae"]
-    activations = ["elu"]
-
-    n_feat_list = (min(n_feat // 5, 50), max(n_feat // 2, 500))
-
-    archs = []
-    for nf in n_feat_list:
-        if n_samples < 2000:
-            archs += [
-                (nf, [[nf * 2], [nf], [nf // 4], [nf // 4]]),
-                (nf, [[nf // 2], [nf // 2], [nf // 4], [nf // 4]]),
-            ]
-        else:
-            archs += [
-                (nf, [[nf * 2], [nf, nf], [nf // 4], [nf // 4]]),
-                (nf, [[nf // 2], [nf // 2, nf // 2], [nf // 4], [nf // 4]]),
-            ]
-
-    hyperparam_presets = []
-    for bs, lr, a, e, l, act in itertools.product(
-        batch_sizes, learning_rates, archs, epochs, losses, activations
-    ):
-        preset = {
-            "batch_size": bs,
-            "lr": lr,
-            "n_feat": a[0],
-            "num_neurons": a[1],
-            "epochs": e,
-            "loss": l,
-            "act": act,
-        }
-        hyperparam_presets.append(preset)
-
-    return hyperparam_presets
+hyperparam_presets = []
+for bs in batch_sizes:
+    for lr in learning_rates:
+        for a in archs:
+            for e in epochs:
+                for l in losses:
+                    for act in activations:
+                        preset = {
+                            "batch_size": bs,
+                            "lr": lr,
+                            "n_feat": a[0],
+                            "num_neurons": a[1],
+                            "epochs": e,
+                            "loss": l,
+                            "act": act,
+                        }
+                        hyperparam_presets.append(preset)
