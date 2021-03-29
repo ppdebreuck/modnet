f30fa826372aa5d4f5caa277285cfc2d5cf1b0fa
Greg <gregoireheymans@gmail.com>
2021-03-29 12:49:00 +0200

    removing @tf.function

--- modnet/fit_genetic.py
+++ modnet/fit_genetic.py
@@ -4,7 +4,6 @@ import random
 from typing import List, Optional
 from random import randint
 import tensorflow.keras as keras
-import tensorflow as tf
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import KFold
@@ -215,7 +214,6 @@ class FitGenetic:
 
         self.fitness = []
         j = 0
-        @tf.function(experimental_relax_shapes=True)
         es = keras.callbacks.EarlyStopping(
             monitor="loss",
             min_delta=0.001,
