45a6080d56937f19428e22352eac0065c67254e2
Greg <gregoireheymans@gmail.com>
2021-03-30 13:00:35 +0200

    correcting randomness

--- modnet/individual.py
+++ modnet/individual.py
@@ -3,44 +3,34 @@ from random import randint
 from modnet.preprocessing import MODData
 import numpy as np
 
-
 class Individual:
+    
     """Class containing each of the tuned hyperparameters for the genetic algorithm.
     """
-
-    xscale_list = ['minmax', 'standard']
-    lr_list = [0.01, 0.005, 0.001]
-    initial_batch_size_list = [8, 16, 32, 64, 128]
-    fraction_list = [1, 0.75, 0.5, 0.25]
-   
-    activation = 'elu'
-    loss = 'mae'
-    n_neurons_first_layer = 32*randint(1,10)
-    fraction1 = random.choice(fraction_list)
-    fraction2 = random.choice(fraction_list)
-    fraction3 = random.choice(fraction_list)
-    xscale = random.choice(xscale_list)
-    lr = random.choice(lr_list)
-    initial_batch_size = random.choice(initial_batch_size_list)
     
+    def __init__(self, data):
+        self.xscale_list = ['minmax', 'standard']
+        self.lr_list = [0.01, 0.005, 0.001]
+        self.initial_batch_size_list = [8, 16, 32, 64, 128]
+        self.fraction_list = [1, 0.75, 0.5, 0.25]
 
-    def n_feat(
-        self,
-        data: MODData
-        ):
-        
-        """Optimal number of features chosen on a sliding scale based on available features.
-        """
+        self.activation = 'elu'
+        self.loss = 'mae'
+        self.n_neurons_first_layer = 32*randint(1,10)
+        self.fraction1 = random.choice(self.fraction_list)
+        self.fraction2 = random.choice(self.fraction_list)
+        self.fraction3 = random.choice(self.fraction_list)
+        self.xscale = random.choice(self.xscale_list)
+        self.lr = random.choice(self.lr_list)
+        self.initial_batch_size = random.choice(self.initial_batch_size_list)
 
-        n_features = 0 #initialization
+        self.n_features = 0 #initialization
         if len(data.get_optimal_descriptors()) <= 100:
             b = int(len(data.get_optimal_descriptors())/2)
-            n_features = randint(1, b) + b
+            self.n_features = randint(1, b) + b
         elif len(data.get_optimal_descriptors()) > 100 and len(data.get_optimal_descriptors()) < 2000:
             max = len(data.get_optimal_descriptors())
-            n_features = 10*randint(1,int(max/10))
+            self.n_features = 10*randint(1,int(max/10))
         else:
             max = np.sqrt(len(data.get_optimal_descriptors()))
-            n_features = randint(1,max)**2
-    
-        return n_features
+            self.n_features = randint(1,max)**2
