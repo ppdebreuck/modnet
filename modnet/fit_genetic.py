ecce3758e643ccb14705602337b5d5eae826ba5c
Greg <gregoireheymans@gmail.com>
2021-03-30 15:12:18 +0200

    typo

--- modnet/fit_genetic.py
+++ modnet/fit_genetic.py
@@ -138,7 +138,7 @@ class FitGenetic:
         self.pop =  [[]]*size_pop
 
         for i in range(0, 10):
-            individual = Individual(md_train)
+            individual = Individual(data)
             self.pop[i] = [individual.n_features, individual.n_neurons_first_layer, individual.fraction1, individual.fraction2, individual.fraction3, individual.activation, individual.loss, individual.xscale, individual.lr, individual.initial_batch_size]
         return self.pop
 
