1717206198021ddffda92b62cb1bd515ef643ab3
Greg <gregoireheymans@gmail.com>
2021-04-21 18:19:26 +0200

    fixing issues

--- modnet/fit_genetic.py
+++ modnet/fit_genetic.py
@@ -337,7 +337,7 @@ class FitGenetic:
                 }
             ]
 
-        a = self.mae_of_individual(tasks)
+        a = self.mae_of_individual(tasks["individual"], tasks["fold"], tasks["individual_id"], tasks["fold_id"])
         print('tasks =', tasks)
         for res in tqdm.tqdm(
                 pool.imap_unordered(self._mae_of_individual, tasks, chunksize=1),
