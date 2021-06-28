cf8a88dd8e95e2631c95c101a831dee34247baf6
ppdebreuck <pierre-paul.debreuck@uclouvain.be>
2021-06-28 20:32:01 +0200

    add vanilla post-process

--- modnet/models/vanilla.py
+++ modnet/models/vanilla.py
@@ -288,6 +288,10 @@ class MODNetModel:
         else:
             validation_data = None
 
+        # set up bounds for postprocessing
+        self.min_y = training_data.df_targets.values.min(axis=0)
+        self.max_y = training_data.df_targets.values.max(axis=0)
+
         # Optionally set up print callback
         if verbose:
             if val_fraction > 0 or validation_data:
@@ -326,6 +330,7 @@ class MODNetModel:
         }
 
         self.model.compile(
+            run_eagerly=True,
             loss=loss,
             optimizer=tf.keras.optimizers.Adam(lr=lr),
             metrics=metrics,
@@ -549,6 +554,15 @@ class MODNetModel:
         p = np.array(self.model.predict(x))
         if len(p.shape) == 2:
             p = np.array([p])
+
+        # post-process based on training data
+        yrange = self.max_y-self.min_y
+        upper_bound = self.max_y + 0.25 * yrange
+        lower_bound = self.min_y - 0.25 * yrange
+        for i,vals in enumerate(p):
+            out_of_range_idxs = np.where((vals < lower_bound[i]) | (vals > upper_bound[i]))
+            vals[out_of_range_idxs] = np.random.uniform(0, 1, size=len(out_of_range_idxs[0])) * (self.max_y[i] - self.min_y[i]) + self.min_y[i]
+
         p_dic = {}
         for i, name in enumerate(self.targets_flatten):
             if self.num_classes[name] >= 2:
