954a451d56782eea9111cb280cb586e34757b605
ppdebreuck <pierre-paul.debreuck@student.uclouvain.be>
2022-04-07 16:53:13 +0200

    tensorflow cpu dependence

--- setup.py
+++ setup.py
@@ -35,8 +35,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         "pandas>=0.25.3",
-        "tensorflow>=2.4",
-        "tensorflow-probability>=0.12",
+        "tensorflow-cpu>=2.4",
         "pymatgen>=2020,<2020.9",
         "matminer>=0.6.2",
         "numpy>=1.18.3",
