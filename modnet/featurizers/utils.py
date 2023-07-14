a408f0ac02120fc63b9f1987d618deb2583958f6
gbrunin <guillaume.brunin@uclouvain.be>
2023-07-14 09:04:03 +0200

    Possibility to remove all NaNs features or not after featurization.

--- modnet/featurizers/utils.py
+++ modnet/featurizers/utils.py
@@ -3,12 +3,13 @@ import numpy as np
 __all__ = ("clean_df",)
 
 
-def clean_df(df):
+def clean_df(df, drop_allnan: bool = True):
     """Cleans dataframe by dropping missing values, replacing NaN's and infinities
     and selecting only columns containing numerical data.
 
     Args:
         df (pd.DataFrame): the dataframe to clean.
+        drop_allnan: if True, clean_df will remove features that are fully NaNs.
 
     Returns:
         pandas.DataFrame: the cleaned dataframe.
@@ -16,7 +17,8 @@ def clean_df(df):
     """
 
     df = df.select_dtypes(include="number")
-    df = df.dropna(axis=1, how="all")
+    if drop_allnan:
+        df = df.dropna(axis=1, how="all")
     df = df.replace([np.inf, -np.inf, np.nan], np.nan)
 
     return df
